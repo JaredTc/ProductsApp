8d734e9bb053ee04ad4bbf6c9b0b2b2ec3157822
Jared Tc <75559684+JaredTc@users.noreply.github.com>
2022-09-17 23:25:54 -0500

    creacion de screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/widgets/product_card.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product.dart';
+import '../services/auth_service.dart';
+import '../services/products_service.dart';
 
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-     ),
+     final productsService = Provider.of<ProductsService>(context);
+     final authService = Provider.of<AuthService>(context, listen: false);
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Products')),
+        actions:  [
+          TextButton(onPressed: () {
+            authService.logout();
+            Navigator.pushReplacementNamed(context, 'login');
+          } , child: Icon(Icons.login_outlined, color: Colors.white,))
+          
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: productsService.products.length,
+        itemBuilder: (BuildContext context, int index) => GestureDetector(
+          onTap: (){
+            productsService.selectedProduct = productsService.products[index].copy();
+            Navigator.pushNamed(context, 'product');
+          },
+          child: ProductCard(
+            product: productsService.products[index],),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add ),
+        onPressed: () {
+
+          productsService.selectedProduct =  Product(
+            available: false, 
+            name: '', 
+            price: 0
+          );
+          Navigator.pushNamed(context, 'product');
+        },
+      ),
    );
   }
 }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,70 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:products_app/widgets/auth_background.dart';
-import 'package:products_app/widgets/card_container.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/login_form_provider.dart';
 import '../services/auth_service.dart';
-import '../services/notifications_service.dart';
 import '../ui/input_decorations.dart';
+import '../widgets/auth_background.dart';
+import '../widgets/card_container.dart';
 
 
-class LoginScreen extends StatelessWidget {
+class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: AuthBackground(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const SizedBox( height: 220,),
-              CardContainer(child: Column(
-                children: [
 
-                  SizedBox( height: 20,),
-                  Text('Login',
-                  style: Theme.of(context).textTheme.headline4,),
-                  const SizedBox( height: 30,),
-                  
-                  ChangeNotifierProvider(
-                    create: ((context) => LoginFormProvider()),
-                    child: _LoginForm(),)
-                ],
-              ) ),
-              const SizedBox( height: 50,),
-              TextButton(onPressed: (){
-                  Navigator.pushReplacementNamed(context, 'register'); 
-              },
+              SizedBox( height: 220 ),
+
+              CardContainer(
+                child: Column(
+                  children: [
+
+                    SizedBox( height: 10 ),
+                    Text('Create Account', style: Theme.of(context).textTheme.headline4 ),
+                    SizedBox( height: 30 ),
+                    
+                    ChangeNotifierProvider(
+                      create: ( _ ) => LoginFormProvider(),
+                      child: _LoginForm()
+                    )
+                    
+
+                  ],
+                )
+              ),
+
+              SizedBox( height: 50 ),
+              TextButton(
+                onPressed: () => Navigator.pushReplacementNamed(context, 'login'), 
                 style: ButtonStyle(
                   overlayColor: MaterialStateProperty.all( Colors.indigo.withOpacity(0.1)),
                   shape: MaterialStateProperty.all( StadiumBorder() )
                 ),
-                child: const Text('Create a new account', style: TextStyle( fontSize: 18, color: Colors.black87 ),
-                )
-                )
+                child: Text('Sign In', style: TextStyle( fontSize: 18, color: Colors.black87 ),)
+              ),
+              SizedBox( height: 50 ),
             ],
           ),
-        ),
+        )
       )
    );
   }
 }
 
-class _LoginForm extends StatelessWidget {
 
+class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     final loginForm = Provider.of<LoginFormProvider>(context);
 
     return Container(
       child: Form(
         key: loginForm.formKey,
         autovalidateMode: AutovalidateMode.onUserInteraction,
+
         child: Column(
           children: [
-              TextFormField(
+            
+            TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecorations.authInputDecoration(
@@ -80,45 +88,48 @@ class _LoginForm extends StatelessWidget {
                   
                   return regExp.hasMatch(value ?? '')
                     ? null
-                    : 'write the correct email';
+                    : 'El valor ingresado no luce como un correo';
 
               },
             ),
-            const SizedBox( height: 30 ),
+
+            SizedBox( height: 30 ),
+
             TextFormField(
               autocorrect: false,
               obscureText: true,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecorations.authInputDecoration(
                 hintText: '*****',
-                labelText: 'password',
-                prefixIcon: Icons.password
+                labelText: 'Password',
+                prefixIcon: Icons.lock_outline
               ),
-              onChanged: ( value )  => loginForm.password = value,
+              onChanged: ( value ) => loginForm.password = value,
               validator: ( value ) {
 
                   return ( value != null && value.length >= 6 ) 
                     ? null
-                    : 'the password should be the 6 characters';                                    
+                    : 'La contraseña debe de ser de 6 caracteres';                                    
                   
               },
             ),
-            const SizedBox( height: 30 ),
+
+            SizedBox( height: 30 ),
+
             MaterialButton(
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
               disabledColor: Colors.grey,
               elevation: 0,
               color: Colors.deepPurple,
               child: Container(
-                padding: EdgeInsets.symmetric( horizontal: 80, vertical: 15),  
+                padding: EdgeInsets.symmetric( horizontal: 80, vertical: 15),
                 child: Text(
                   loginForm.isLoading 
-                    ? 'Espere'
-                    : 'Ingresar',
+                    ? 'Wait'
+                    : 'Register',
                   style: TextStyle( color: Colors.white ),
                 )
               ),
-
               onPressed: loginForm.isLoading ? null : () async {
                 
                 FocusScope.of(context).unfocus();
@@ -129,15 +140,14 @@ class _LoginForm extends StatelessWidget {
                 loginForm.isLoading = true;
 
 
-                // // TODO: validar si el login es correcto
-                final String? errorMessage = await authService.login(loginForm.email, loginForm.password);
+                // TODO: validar si el login es correcto
+                final String? errorMessage = await authService.createUser(loginForm.email, loginForm.password);
 
                 if ( errorMessage == null ) {
                   Navigator.pushReplacementNamed(context, 'home');
                 } else {
-                //   // TODO: mostrar error en pantalla
-                  // print( errorMessage );
-                  NotificationsService.showSnackbar(errorMessage);
+                  // TODO: mostrar error en pantalla
+                  print( errorMessage );
                   loginForm.isLoading = false;
                 }
               }
